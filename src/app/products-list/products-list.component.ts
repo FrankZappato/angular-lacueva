af41815f97a3598c5743c853c4b59f3743a870c3
Francisco <franciscovaninetti@gmail.com>
2020-11-12 08:46:12 -0300

    implements of pipes and directives

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -13,21 +13,24 @@ export class ProductsListComponent implements OnInit {
     category : "Processor",
     price : 2300,
     stock : 0,
-    image : "assets/img/ryzen-7.jpg"
+    image : "assets/img/ryzen-7.jpg",
+    clearance : false,
   },
   {
     name : "Microphone Shure",
     category : "Microphone",
     price : 750,
     stock : 12,
-    image : "assets/img/ryzen-7.jpg"
+    image : "assets/img/ryzen-7.jpg",
+    clearance : true,
   },
   {
     name : "Ram Memory Corsair",
     category : "Ram memory",
     price : 1250,
     stock : 18,
-    image : "assets/img/ryzen-7.jpg"
+    image : "assets/img/ryzen-7.jpg",
+    clearance : false,
   }
 ];
   constructor() { }
